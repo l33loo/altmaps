f31d22ada5a76cfe04d3b9cd381b297d6b31c7f0
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-05 23:22:36 +0000

    Edit seeds to reflect data in maps table

--- db/seeds/favorite_maps.js
+++ db/seeds/favorite_maps.js
@@ -2,16 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('favorite_maps').del()
     .then(function () {
       return Promise.all([
-        knex('favorite_maps').insert({id: 1, map_id: 1, user_id: 2}),
-        knex('favorite_maps').insert({id: 2, map_id: 2, user_id: 3}),
-        knex('favorite_maps').insert({id: 3, map_id: 3, user_id: 1}),
-        knex('favorite_maps').insert({id: 4, map_id: 4, user_id: 2}),
-        knex('favorite_maps').insert({id: 5, map_id: 5, user_id: 3}),
-        knex('favorite_maps').insert({id: 6, map_id: 1, user_id: 1}),
-        knex('favorite_maps').insert({id: 7, map_id: 2, user_id: 2}),
-        knex('favorite_maps').insert({id: 8, map_id: 3, user_id: 3}),
-        knex('favorite_maps').insert({id: 9, map_id: 4, user_id: 1}),
-        knex('favorite_maps').insert({id: 10, map_id: 5, user_id: 2})
+        knex('favorite_maps').insert({id: 1, map_id: 1, user_id: 1}),
+        knex('favorite_maps').insert({id: 2, map_id: 1, user_id: 2}),
+        knex('favorite_maps').insert({id: 3, map_id: 1, user_id: 3})
       ]);
     });
 };
--- db/seeds/maps.js
+++ db/seeds/maps.js
@@ -1,12 +1,8 @@
 exports.seed = function(knex, Promise) {
   return knex('maps').del()
-    .then(function() {
+    .then(function () {
       return Promise.all([
-        knex('maps').insert({id: 1, created_by: 1, latitude: 48.4064791, longitude: -123.3735108, title: "I am a beach", description: "This is the first place I clicked"}),
-        knex('maps').insert({id: 2, created_by: 2, latitude: 48.4335921, longitude: -123.3123138, title: "Willows Beach", description: "Awesome swimming!!1"}),
-        knex('maps').insert({id: 3, created_by: 3, latitude: 48.463765, longitude: -123.280170, title: "Telegraph Cove", description: "So romantic! Awww"}),
-        knex('maps').insert({id: 4, created_by: 4, latitude: 48.4555994, longitude: -123.350961, title: "Water Feature", description: "Surprise, it's not actually a beach! Do not swim in the water features!"}),
-        knex('maps').insert({id: 5, created_by: 5, latitude: 48.4465182, longitude: -123.4077808, title: "That Beach on The Gorge", description: "Secluded Solitude, rewards await the adventurous swimmer"})
+        knex('maps').insert({id: 1, created_by: 1, title: 'foo', description: 'foo'}),
       ]);
     });
-}
+};
