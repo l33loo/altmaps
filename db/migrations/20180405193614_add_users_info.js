891c2625682ad2052796ac7960493767a5d60df6
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-05 20:00:44 +0000

    Add .DS_Store

---
+++ db/migrations/20180405193614_add_users_info.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.withSchema('public').table('users', function(table) {
+      table.renameColumn('name', 'first_name');
+      table.string('last_name');
+      table.string('email').unique();
+      table.string('password_hash');
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('users', function(table) {
+      table.renameColumn('first_name', 'name');
+      table.dropColumn('last_name');
+      table.dropColumn('email');
+      table.dropColumn('password_hash');
+      table.dropTimestamps();
+    })
+  ])
+};
