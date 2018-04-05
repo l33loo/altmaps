ff40cef4dfa21bbff9c5e5b9ada0f79669f19340
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-05 21:02:26 +0000

    Create map pins table

---
+++ db/migrations/20180405204829_create_map_pins_table.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.withSchema('public').createTable('map_pins', function(table) {
+      table.increments();
+      table.integer('user_id').unsigned();
+      table.foreign('user_id').references('users.id');
+      table.integer('map_id').unsigned();
+      table.foreign('map_id').references('maps.id');
+      table.string('place_id');
+      table.string('latitude');
+      table.string('longitude');
+      table.string('title');
+      table.text('description');
+      table.string('category');
+      table.string('color');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('map_pins')
+  ])
+};
