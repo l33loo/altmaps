35018350e69a41352e6157a380f43060ee7b81b0
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-07 06:40:20 +0000

    Begin work on profile page

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,26 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+  router.get("/:id/json", (req, res) => {
+    knex
+      .select("*")
+      .from("maps")
+      .fullOuterJoin("favorite_maps", "maps.created_by", "favorite_maps.user_id")
+      .where({user_id: req.params.id})
+      .then((results) => {
+        res.json(results);
+      });
+  });
+
   // VIEW USER PROFILE
   router.get("/:userID", (req, res) => {
-    // if (/* userID exists*/) {
-      res.render("user");
-    // } else {
+    // if (exists) {
+      // if (loggedIn) { //
+        res.render("user", {loggedIn: req.loggedIn, userId: req.session.userId});
+      // } else {
+        // res.status(40).send -- unauthorized, need to log in
+      // }
+    // } else {}
     //   res.status(404).send("<html><body>This user does not exist. Please try again.</body></html>\n");
     // }
   });
