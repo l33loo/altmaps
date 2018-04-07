69dcf5f3b6af415911092dd353cd0dd3883cf790
Andrew Barclay <andrewkbarclay@gmail.com>
2018-04-07 05:02:51 +0000

    added heart toggle to map cards

--- public/scripts/list-maps.js
+++ public/scripts/list-maps.js
@@ -1,48 +1,21 @@
 $(document).ready(function(){
 
   function createMapCard(id, title, created_by, updated_at, description){
-    // var $link = $('<a class="card" href="/maps/' + id + '" ></a>"');
-    // console.dir($link);
+
     var $card = $('<div class="card">').data({mapId: id});
     $('<div class="image"><img src="/images/map_placeholder.jpg"></div>').appendTo($card);
     var $content = $('<div class="content">').appendTo($card);
     var $title = $('<div class="header">').text(title).appendTo($content);
-    var $favourite = $('<span class="right floated"><i class="heart outline icon"></span>').appendTo($title);
+    var $favourite = $('<span class="right floated"><i class="heart outline icon"></i></span>').appendTo($title);
     var $description = $('<div class="description">').text(description).appendTo($content);
 
     var $extraContent = $('<div class="extra content">').appendTo($card);
     var $rightFooter = $('<span class="right floated">').text(updated_at).appendTo($extraContent);
     var $leftFooter = $('<span>').text("75 Contributors").appendTo($extraContent);
-    var $icon = $('<i class="user icon"></i><i class="heart icon"></i><i class="heart outline icon"></i>').insertBefore($leftFooter);
-
-
-
+    var $icon = $('<i class="user icon"></i>').insertBefore($leftFooter);
 
     return $card;
 
-      //<div class="card">
-      // <div class="image">
-      //   <img src="/images/map_placeholder.jpg">
-      // </div>
-      // <div class="content">
-      //   <div class="header">I'm a cool map</div>
-      //   <div class="meta">
-      //     <a>Friends</a>
-      //   </div>
-      //   <div class="description">
-      //     This is a map of the coolest things.
-      //   </div>
-      // </div>
-      // <div class="extra content">
-      //   <span class="right floated">
-      //     Created today!
-      //   </span>
-      //   <span>
-      //     <i class="user icon"></i>
-      //     75 Contributors
-      //   </span>
-      // </div>
-      //</div>
   }
 
   function populateMapList(maps, $parent){
@@ -53,10 +26,25 @@ $(document).ready(function(){
 
   }
 
+// toggles the "like" heart immediately in the DOM, and also updates database in background
+function toggleFavourite($favourite){
+  $favourite.toggleClass("outline red");
+  // $favourite.append('<div class="ui red tag label">Favourited!</div>');
+
+  // this needs to set up a post request eventually to store the favourite in the database.
+  // let id = $like.data("mongo-id");
+  // $.post(`/tweets/${id}/like`, "");
+}
+
   function getMaps(){
     $.getJSON("/maps/json").then(function(maps){
 
       populateMapList(maps, $('#maps-list'));
+
+      $("div.card i.heart").on("click", function(event){
+        event.stopPropagation();
+        toggleFavourite($(this));
+      });
 
       $('div.card').on("click", function(){
         window.location.href = "/maps/" + $(this).data().mapId;
