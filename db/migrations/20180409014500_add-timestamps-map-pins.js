7536c2c4bc92951dfde1512b18008344232feeec
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-09 01:54:23 +0000

    Add timestamps to map_pins db

---
+++ db/migrations/20180409014500_add-timestamps-map-pins.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.withSchema('public').table('map_pins', function(table) {
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.withSchema('public').table('map_pins', function(table) {
+      table.dropColumn('created_at');
+      table.dropColumn('updated_at');
+    })
+  ])
+};
